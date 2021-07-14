8c945874a2d10522509920a657385e1f2eb56052
Sophia <justanothersophie@gmail.com>
2021-07-14 11:15:13 -0400

    commit tree 98994896c89436da35030a21e36007a19da8b21d

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(input)
+  output=String.new
+  if input % 3 == 0  # if remainder is 0 when divided by 3
+    output="Fizz"  # i.e. if divisible by 3
+  end
+  if input % 5 == 0
+    output=output+"Buzz"
+  end
+  if output == String.new
+    output=nil
+  end
+  output
+end
